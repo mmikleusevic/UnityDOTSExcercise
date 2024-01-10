c34d088f19b6e73d81aec70f78845394b87cc86f
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-01-10 19:33:59 +0100

    Added Job with burst compile

--- Assets/Scripts/RotatingCubeSystem.cs
+++ Assets/Scripts/RotatingCubeSystem.cs
@@ -1,3 +1,4 @@
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 
@@ -8,11 +9,34 @@ public partial struct RotatingCubeSystem : ISystem
         state.RequireForUpdate<RotateSpeed>();
     }
 
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>,RefRO<RotateSpeed>>())
+        //foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>,RefRO<RotateSpeed>>())
+        //{
+        //    localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+        //}
+
+        RotatingCubeJob rotatingCubeJob = new RotatingCubeJob
         {
-            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+
+        //If IJobEntity then like this, also if you use ScheduleParallel() it splits across multiple worker threads only after 128 entities, till that it keeps it on one
+        rotatingCubeJob.Schedule();
+
+        //If IJobFor or anything else
+        //state.Dependency = rotatingCubeJob.Schedule(state.Dependency);
+    }
+
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+            localTransform = localTransform.RotateY(rotateSpeed.value * deltaTime);
         }
     }
 }
